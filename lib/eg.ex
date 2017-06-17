e5a2297f69899cf4a2f40bd19a4576482b701245
Rodolfo Yabut <rodolfoyabut@gmail.com>
2017-06-17 06:52:53 -0700

    Init

---
+++ lib/eg.ex
@@ -0,0 +1,18 @@
+defmodule Eg do
+  @moduledoc """
+  Documentation for Eg.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Eg.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/eg_test.exs
@@ -0,0 +1,8 @@
+defmodule EgTest do
+  use ExUnit.Case
+  doctest Eg
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
