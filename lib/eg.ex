f940a56aedc2ee49de5bf3c8f5d14ef20edf367e
Rodolfo Yabut <rodolfoyabut@gmail.com>
2017-06-17 06:57:35 -0700

    Start/Stop GenServer boilerplate

--- lib/eg.ex
+++ lib/eg.ex
@@ -4,8 +4,25 @@ defmodule Eg do
   @name Mango
 
   ## Client API
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, opts ++ [name: Mango])
+  end
 
+  def stop do
+    GenServer.cast(@name, :stop)
+  end
   ## Callbacks
+  def init(:ok) do
+    {:ok, %{}}
+  end
 
+  def handle_cast(:stop, state) do
+    {:stop, :normal, state}
+  end
+
+  def terminate(reason, state) do
+    IO.puts("Server terminated - #{inspect reason}")
+    :ok
+  end
   ## Helpers
 end
